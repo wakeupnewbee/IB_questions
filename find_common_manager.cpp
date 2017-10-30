133648300db03fbcfecd642aa0b68a38f53403cc
wakeupnewbee <32076994+wakeupnewbee@users.noreply.github.com>
2017-10-30 16:41:24 -0400

    Update find_common_manager.cpp

--- find_common_manager.cpp
+++ find_common_manager.cpp
@@ -4,27 +4,14 @@
 using namespace std;
 
 class sort{
-    /*
-    string name;
-    sort *myself;
-    sort *manager;
-    sort *next;
-    */
     public:
-    sort(string _name, sort* body, sort* tail): name(_name),myself(body),manager(tail){}
+    sort(string _name):name(_name){}
+    //sort(string _name, sort* _manager): name(_name),manager(_manager){}
     
+    void set_tail(sort * tail){ manager = tail;}
     string name;
-    sort *myself;
     sort *manager;
-    
-    /*
-    void set_name(string _name) {name=_name;}
-    void set_pname(sort * p_name) { myself = p_name;}
-    void set_pmanager(sort * p_manager) {manager = p_manager;}
-    
-    sort * show_name(){ return myself;}
-    sort * show_manager(){ return manager;}
-    */
+    sort *self;
 };
 
 class lookup{
@@ -35,9 +22,22 @@ class lookup{
     lookup(){}
     lookup(string _name): name(_name){}
     void set(string _name, lookup * _next){name = _name, next = _next;}
+    void set_addr(sort * _address){address= _address;}
 };
 
-lookup* process(string line){
+sort * find_name(lookup * head,string name){
+    lookup * test = head;
+    while(test != NULL){
+        if(test->name != name){
+            test = test->next;
+        }else{
+            return test->address;
+        }
+    }
+    return NULL;
+}
+
+lookup* process(string line, string & n1, string & n2){
     lookup * head = NULL;
     lookup * tail = NULL;
     lookup * tmp1 = NULL;
@@ -46,9 +46,13 @@ lookup* process(string line){
     size_t pos=0;
     size_t inner_pos=0;
     
-    string name1,name2;//look up name
     string manager="??",worker=" ??";
     string part;
+    
+    sort *sworker;
+    sort *smanager;
+    sort *tmpw;
+    sort *tmpm;
     
     while((pos=line.find(deli1))!=string::npos){
         
@@ -62,16 +66,43 @@ lookup* process(string line){
                 head = new lookup(manager);
                 tmp1 = new lookup(worker);
                 head->next = tmp1;
-                tmp1->next = new lookup;
-                tail = tmp1->next;
+                tail = tmp1;
+                tail->next = NULL;
+                sworker=new sort(worker);
+                smanager=new sort(manager);
+                sworker->set_tail(smanager);
+                head->set_addr(smanager);
+                tmp1->set_addr(sworker);
             }else{
-                tmp1 = new lookup(worker);
-                tail->set(manager,tmp1);
-                tmp1->next = new lookup;
-                tail = tmp1->next;
+                //sworker=new sort(worker);
+                //smanager=new sort(manager);
+                //sworker->set_tail(smanager);
+                if((tmpm=find_name(head,manager))==NULL){// new manager comes
+                    tmp1 = new lookup(manager);
+                    tail->next = tmp1;
+                    tmp1->next = NULL;
+                    tail = tmp1;
+                    tmp1->set_addr(smanager);
+                    smanager=new sort(manager);
+                }
+                if((tmpw=find_name(head,worker))==NULL){//new works comes :D who case who is his manager
+                    tmp1 = new lookup(worker);
+                    tail->next = tmp1;
+                    tmp1->next =NULL;
+                    tail = tmp1;
+                    tmp1->set_addr(sworker);
+                    sworker=new sort(worker);
+                }
+                if(tmpm!=NULL){
+                    sworker->set_tail(tmpm);
+                }else if(tmpw != NULL){
+                    smanager->set_tail(tmpw);
+                }else{
+                    sworker->set_tail(smanager);
+                }
             }
         }else{
-            name1 = part;
+            n1 = part;
         }
         
         line.erase(0,pos + deli1.length());
@@ -79,66 +110,75 @@ lookup* process(string line){
     }
 
     //line.erase(line.size()-1); no change line, no need, be aware
-    name2 = line;
+    n2 = line;
     return head;
 }
 
-int * ret(){
-    int x = 1;
-    int *y = &x;
-    return y;
+void finding(sort * head1, sort * head2){
+    head1 = head1->manager;
+    head2 = head2->manager;
+    sort * tmp = head1;
+    
+    while(head2 != NULL){
+        
+        while(tmp !=NULL){
+            cout << "checking "<<endl;
+            cout << tmp->name << endl;
+            cout << head2->name <<endl;
+            if(tmp->name != head2->name){
+                tmp=tmp->manager;
+            }else{
+                cout <<"almost"<<endl;
+                cout << head1->name<<endl;;
+                return;
+            }
+            //cout << tmp->name <<endl;
+        }
+        cout << "finish on loop" <<endl;
+        tmp = head1;
+        head2 = head2->manager;
+    }
 }
 
 int main()
 {
-    int obj=10;
-    int * p1=&obj;
-    int * p2=&obj;
-    if(p1==p2){
-        cout << "two points same obj"<<endl;
-    }
-    *p1+=1;
-    cout<<"p1 is changing to"<< *p1<< " so p2 = ?  :"<< *p2<<endl;
-    *p2-=10;
-    cout<<"p2 is changing to"<< *p2<< " so p1 = ?  :"<< *p1<<endl;
-    
-    sort mei("mei",&mei,NULL);
-    sort bob("bob",&bob,&mei);
-    sort jim("jim",&jim,&mei);
-    
-    if(jim.manager->name == bob.manager->name){
-        string boss;
-        boss= jim.manager->name;
-        cout << boss << endl;
-    }
-    
-    sort * tmp = new sort("luck1",tmp,NULL);
-    sort * A = tmp;
-    cout<< tmp << " " << A <<endl;
-    tmp = new sort("luck2",tmp,A);
-    sort * B = tmp;
-    cout<< tmp << " " << B <<endl;
-    tmp = new sort("luck3",tmp,B);
-    sort * C = tmp;
-    cout<< tmp << " " << C <<endl;
-    
-    cout << A->manager <<" " 
-         << B->name <<" " 
-         << C->name <<endl;
-    
-    delete A,B,C,tmp;
-    
     string line;
+    string n1,n2;
     getline(cin,line);
-    lookup * head = process(line);
     
-    while(head!= NULL){
-        cout << head->name << endl;
-        head = head->next;
+    lookup * head = process(line,n1,n2);
+    cout <<"This is the names i need to work on: "<< n1 << " "<< n2<< endl;
+    
+    cout << "the lookup list: "<<endl;
+    
+    lookup * find = head; 
+    while(find!= NULL){
+        cout << find->name << endl;
+        find = find->next;
     }
-    //}
-    int * i = new int;
+    
+    find = head;
+    sort *find1;
+    sort *find2;
+    while(find != NULL){
+        if(find->name == n1){
+            find1 = find->address;
+        }
+        if(find->name == n2){
+            find2 = find->address;
+        }
+        find = find->next;
+    }
+    cout <<"out of whiling"<<endl;
+    /*while(find2!=NULL){
+        cout << find2->name <<endl;
+        find2 = find2->manager;
+    }*/
+    finding(find1,find2);
     
     
    return 0;
 }
+
+
+//Frank->Mary,Mary->Sam,Mary->Bob,Sam->Katie,Sam->Pete,Bob->John,Bob,Katie
